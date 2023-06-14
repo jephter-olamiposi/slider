68bed66460ee2d486c5d6120a66a613aee123b95
jephter-olamiposi <grandbusta@gmail.com>
2023-06-14 23:18:16 +0100

    slide adjustment

--- index.js
+++ index.js
@@ -47,39 +47,35 @@ document.querySelector('.gg').innerHTML = `
 </div>`;
 
 
+
 function slidenext() {
     if (counter === allSlides.length - 1) {
-        counter = -1;
+        counter=0;
+        setTimeout(() => { displaySlide(false) }, 100);
     }
-
-    if (counter < allSlides.length - 1) {
+    else {
         counter++
         document.querySelector('.container').classList.add('animate-go')
-        setTimeout(() => { displaySlide(true) }, 200)
+        setTimeout(() => { displaySlide(true) }, 100)
     }
+
 }
 
 prev.addEventListener('click', slideprev);
 
 function slideprev() {
     if (counter === 0) {
-        counter = 4;
-
+        counter =3 ;
+        setTimeout(() => { displaySlide(true) }, 100)
     }
 
-    if (counter > 0) {
+    else {
         counter--
-
         document.querySelector('.container').classList.add('animate-come')
-        setTimeout(() => { displaySlide(false) }, 200)
+        setTimeout(() => { displaySlide(false) }, 100)
     }
-
-
-
+    
 }
-
-
-
 function displaySlide(isNext) {
     document.querySelector('.gg').innerHTML = `
 <div class="container ${isNext ? 'animate-next' : 'animate-prev'}">
@@ -95,36 +91,10 @@ function displaySlide(isNext) {
 </div>`;
     console.log(counter);
 }
-function auto(isauto) {
-    document.querySelector('.gg').innerHTML = `
-<div class="container ${isauto ? 'slide' : 'slide'}">
-    <div class="img">
-        <img ${allSlides[counter].image} class="person-img">
-    </div>
-    <h5>${allSlides[counter].name}</h5>
-    <p class="title">${allSlides[counter].title}</p>
-    <p class="text">${allSlides[counter].text}</p>
-    <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 512 512" class="icon"height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
-    <path d="M464 32H336c-26.5 0-48 21.5-48 48v128c0 26.5 21.5 48 48 48h80v64c0 35.3-28.7 64-64 64h-8c-13.3 0-24 10.7-24 24v48c0 13.3 10.7 24 24 24h8c88.4 0 160-71.6 160-160V80c0-26.5-21.5-48-48-48zm-288 0H48C21.5 32 0 53.5 0 80v128c0 26.5 21.5 48 48 48h80v64c0 35.3-28.7 64-64 64h-8c-13.3 0-24 10.7-24 24v48c0 13.3 10.7 24 24 24h8c88.4 0 160-71.6 160-160V80c0-26.5-21.5-48-48-48z"></path>
-    </svg>
-</div>`;
-    console.log(counter);
-}
-showSlides();
 
 function showSlides() {
+    slidenext()
 
-
-    let i;
-    for (i = 0; i < allSlides.length - 1; i++) {
-        document.querySelector('.container').classList.add('slide');
-        auto(true);
-    }
-    if (counter === allSlides.length - 1) {
-        counter = -1;
-    }
-    counter++
-
-    setTimeout(showSlides, 5800);
 }
+setInterval(showSlides, 7000);
 
